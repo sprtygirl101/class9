ecba5b31978246beea98122473b59fe8a2b4b34a
sprtygirl101 <86930639+sprtygirl101@users.noreply.github.com>
2021-08-05 17:57:47 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var ball;
+function setup() {
+  createCanvas(400,400);
+  ball = createSprite(200,200,50,50);
+  ball.shapeColor = "red"
+}
+
+function draw() 
+{
+  background(30);
+drawSprites();
+}
+
+
+
+
